51285ba8a0e4ee9d6bac6339b4f4e84534e1c2a9
Daniel Brathagen <dbrathagen@gmail.com>
2022-11-15 19:50:29 +0000

    Solution to tile buffer styling bug

--- src/lib/VectorGrid.js
+++ src/lib/VectorGrid.js
@@ -19,8 +19,25 @@ L.SVG.Tile.include({
 });
 
 // @ts-ignore
+const wrapCoords = L.GridLayer.prototype._wrapCoords;
+console.log(L.GridLayer.prototype)
+
+L.GridLayer.include({
+  _wrapCoords: (coords) => coords,
+})
+
+// @ts-ignore
 const CustomSlicer = L.VectorGrid.Slicer.extend({
   _additionalClassNames: [],
+
+	initialize: function(geojson, options) {
+		// @ts-ignore
+		L.VectorGrid.Slicer.prototype.initialize.call(this, geojson, options);
+	},
+
+  _posToKey: function(pos) {
+    return pos.x + ":" + pos.y
+  },
 
   _createLayer: function(feat, pxPerExtent) {
     // @ts-ignore
@@ -29,9 +46,97 @@ const CustomSlicer = L.VectorGrid.Slicer.extend({
     return layer;
   },
 
+  createTile: function(coords, done) {
+		var storeFeatures = this.options.getFeatureId;
+
+    var rawCoords = coords;
+    coords = wrapCoords.bind(L.GridLayer.prototype)(coords);
+
+		var tileSize = this.getTileSize();
+		var renderer = this.options.rendererFactory(coords, tileSize, this.options);
+
+		var vectorTilePromise = this._getVectorTilePromise(coords);
+
+		if (storeFeatures) {
+			this._vectorTiles[this._tileCoordsToKey(rawCoords)] = renderer;
+			renderer._features = {};
+		}
+
+		vectorTilePromise.then( function renderTile(vectorTile) {
+			for (var layerName in vectorTile.layers) {
+				this._dataLayerNames[layerName] = true;
+				var layer = vectorTile.layers[layerName];
+				console.log(layer, 'layer', vectorTile.coords);
+
+				var pxPerExtent = this.getTileSize().divideBy(layer.extent);
+				console.log(this.getTileSize(), pxPerExtent);
+
+				var layerStyle = this.options.vectorTileLayerStyles[ layerName ] ||
+				L.Path.prototype.options;
+
+				for (var i = 0; i < layer.features.length; i++) {
+					var feat = layer.features[i];
+					var id;
+
+					var styleOptions = layerStyle;
+					if (storeFeatures) {
+						id = this.options.getFeatureId(feat);
+						var styleOverride = this._overriddenStyles[id];
+						if (styleOverride) {
+							if (styleOverride[layerName]) {
+								styleOptions = styleOverride[layerName];
+							} else {
+								styleOptions = styleOverride;
+							}
+						}
+					}
+
+					if (styleOptions instanceof Function) {
+						styleOptions = styleOptions(feat.properties, coords.z);
+					}
+
+					if (!(styleOptions instanceof Array)) {
+						styleOptions = [styleOptions];
+					}
+
+					if (!styleOptions.length) {
+						continue;
+					}
+
+					var featureLayer = this._createLayer(feat, pxPerExtent);
+
+					for (var j = 0; j < styleOptions.length; j++) {
+						var style = L.extend({}, L.Path.prototype.options, styleOptions[j]);
+						featureLayer.render(renderer, style);
+						renderer._addPath(featureLayer);
+					}
+
+					if (this.options.interactive) {
+						featureLayer.makeInteractive();
+					}
+
+					if (storeFeatures) {
+						renderer._features[id] = [{
+							layerName: layerName,
+							feature: featureLayer
+						}, ...(renderer._features[id] ?? [])];
+					}
+				}
+
+			}
+			if (this._map != null) {
+				renderer.addTo(this._map);
+			}
+			L.Util.requestAnimFrame(done.bind(coords, null, null));
+		}.bind(this));
+
+		return renderer.getContainer();
+  },
+
   // ClassName seems to only be set in style when rendering
   // And also overwrites, hence this method acts like the d3 classed method
   setFeatureClass: function (id, className, added) {
+    console.log(this._vectorTiles, 'vt')
     if (added && !this._additionalClassNames[id]?.includes(className)) {
       this._additionalClassNames[id]
         ? this._additionalClassNames[id].push(className)
@@ -46,12 +151,15 @@ const CustomSlicer = L.VectorGrid.Slicer.extend({
       var features = tile._features;
       var data = features[id];
       if (data) {
-        var feat = data.feature;
-        if (added) {
-          feat && tile.addClass(feat, className)
-        } else {
-          feat && tile.removeClass(feat, className)
-        }
+				for (const d of data) {
+					var feat = d.feature;
+					if (added) {
+						console.log(tileKey, className, 'tile', tile, 'feat', feat)
+						feat && tile.addClass(feat, className)
+					} else {
+						feat && tile.removeClass(feat, className)
+					}
+				}
       }
     }
     return this;
